e2388caa344820864cee9bbd1e6f94c66a88bc5a
dem4ply <dem4ply@gmail.com>
2019-03-08 16:18:47 -0600

    change env vars

--- tests/compass/__init__.py
+++ tests/compass/__init__.py
@@ -5,8 +5,8 @@ from tetrapod.compass import connections
 connections.configure(
     default={
         'wsdl': 'tests/compass/DatalinkOrderService.wsdl.xml',
-        'account': os.environ[ 'COMPASS__DEFAULT__ACCOUNT_ID' ],
-        'user': os.environ[ 'COMPASS__DEFAULT__USER_ID' ],
+        'account': os.environ[ 'COMPASS__DEFAULT__ACCOUNT' ],
+        'user': os.environ[ 'COMPASS__DEFAULT__USER' ],
         'password': os.environ[ 'COMPASS__DEFAULT__PASSWORD' ]
     },
 )
